f20fd1b23d480fb0da30deca1bea4fb824f3d8bd
Jesse Martin <jmartin@yammer-inc.com>
2017-03-29 13:25:14 -0700

    use supported http library: HttpWebRequest

--- Assets/Scripts/YammerController.cs
+++ Assets/Scripts/YammerController.cs
@@ -1,22 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using UnityEngine;
+using AssemblyCSharp;
 
 public class YammerController : MonoBehaviour {
-
-	private string BASE_URI = "https://www.yammer.com/";
 	private string INBOX_UNREAD = "api/v2/inboxes?include_thread_starter=true&threads_count=20&messages_count=2&followed_threads=true&thread_read_state=UNREAD&for_feed=inboxUnread&fetch_type=initial";
-	private string BEARER_TOKEN = "6631141-Lyo0n4Z0xpqQCUpkUCpuA";
+
 
 	// Use this for initialization
 	void Start () {
-		var client = new HttpClient ();
-		client.BaseAddress = new Uri (BASE_URI);
-		client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", BEARER_TOKEN);
+		var request = new YammerRequest ("GET", INBOX_UNREAD);
+		System.Net.HttpWebResponse response = request.getResponse ();
 	}
 
 	// Update is called once per frame
