76e022d9c8f2047084774af8810b4d8fae1af628
Julia Ducey <julia.ducey@gmail.com>
2017-03-29 14:08:25 -0700

    Added pointer logic: on enter, on exit, and on click handling.

--- Assets/Scripts/YammerController.cs
+++ Assets/Scripts/YammerController.cs
@@ -1,26 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using UnityEngine;
 
-public class YammerController : MonoBehaviour {
+public class YammerController : MonoBehaviour
+{
 
 	private string BASE_URI = "https://www.yammer.com/";
 	private string INBOX_UNREAD = "api/v2/inboxes?include_thread_starter=true&threads_count=20&messages_count=2&followed_threads=true&thread_read_state=UNREAD&for_feed=inboxUnread&fetch_type=initial";
 	private string BEARER_TOKEN = "6631141-Lyo0n4Z0xpqQCUpkUCpuA";
 
 	// Use this for initialization
-	void Start () {
-		var client = new HttpClient ();
-		client.BaseAddress = new Uri (BASE_URI);
-		client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", BEARER_TOKEN);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
+	void Start ()
+    {
 	}
 }
