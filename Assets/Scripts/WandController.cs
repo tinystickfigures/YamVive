657821f1c7ec850f1c1f1359bd14cffec7bee912
Julia Ducey <julia.ducey@gmail.com>
2017-03-29 11:56:39 -0700

    Initial commit

---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+ 
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index);  } }
+    private SteamVR_TrackedObject trackedObj;
+
+    private GameObject pickup;
+
+    // Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (controller == null)
+        {
+            Debug.Log("Controller not init");
+            return;
+        }
+        if (controller.GetPressDown(gripButton) && pickup != null)
+        {
+            Debug.Log("down");
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().useGravity = false;
+        }
+        if (controller.GetPressUp(gripButton) && pickup != null )
+        {
+            Debug.Log("up");
+            pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = true;
+
+        }
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        pickup = collider.gameObject;
+    }
+    
+    private void OnTriggerExit(Collider collider)
+    {
+        pickup = null;
+    }
+}
