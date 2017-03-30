e1165ef0bac48aa4b53244da075fee0409c995fa
Jesse Martin <jmartin@yammer-inc.com>
2017-03-29 17:52:53 -0700

    replace RestSharp with HttpClient

--- Assets/Scripts/YammerController.cs
+++ Assets/Scripts/YammerController.cs
@@ -3,15 +3,11 @@ using AssemblyCSharp;
 
 public class YammerController : MonoBehaviour {
 	private string INBOX_UNREAD = "api/v2/inboxes?include_thread_starter=true&threads_count=20&messages_count=2&followed_threads=true&thread_read_state=UNREAD&for_feed=inboxUnread&fetch_type=initial";
-    private string GROUP = "api/v2/networks/6631141/feeds/group/11186532?include_thread_starter=true&threads_count=8&messages_count=2&for_feed=group&fetch_type=initial";
-    
+	private string GROUP = "api/v1/messages/in_group/11186532";
+
     // Use this for initialization
     void Start () {
-		var request = new YammerRequest (RestSharp.Method.POST, GROUP);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
+        Debug.Log("STARTTTT");
+        var request = new YammerRequest (GROUP);
 	}
 }
--- Assets/Scripts/YammerRequest.cs
+++ Assets/Scripts/YammerRequest.cs
@@ -1,5 +1,7 @@
-﻿using RestSharp;
-using UnityEngine;
+﻿using UnityEngine;
+using CI.HttpClient;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AssemblyCSharp
 {
@@ -8,20 +10,21 @@ namespace AssemblyCSharp
 		private string BASE_URI = "https://www.yammer.com/";
 		private string BEARER_TOKEN = "6631141-Lyo0n4Z0xpqQCUpkUCpuA";
 
-		public YammerRequest (Method method, string url)
+		public YammerRequest (string url)
 		{
-            var client = new RestClient();
-            client.BaseUrl = BASE_URI;
-            
-            var request = new RestRequest();
-            request.Method = method;
-            request.Resource = url;
-            request.AddHeader("Authorization", "Bearer " + BEARER_TOKEN);
+			ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
 
-            client.ExecuteAsync(request, (response) =>
-            {
-                Debug.Log(response.Content);
-            });
+			HttpClient client = new HttpClient();
+			//client.Certificates.Add(X509Certificate.CreateFromCertFile("/Assets/www.yammer.com.crt"));
+
+			StringContent content = new StringContent ("{}");
+			client.CustomHeaders.Add ("Authorization", "Bearer " + BEARER_TOKEN);
+			client.Headers.Add (HttpRequestHeader.Accept, "application/json");
+
+			client.GetString(new System.Uri(BASE_URI + url), (r) => {
+				var data = r.Data;
+				Debug.Log(data);
+			});
         }
 	}
 }
